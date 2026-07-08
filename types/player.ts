f451047aaed6ade2db32393efbd2eb5cc2200a5d
billevans <bill.sunshine@gmail.com>
2026-07-08 22:04:12 +1000

    Update player.ts

--- types/player.ts
+++ types/player.ts
@@ -1,17 +1,45 @@
 export interface AOE4Player {
 
-  id:string;
+  profile_id:number;
 
   name:string;
 
   country?:string;
 
-  steam_id?:string;
-
   avatar?:string;
 
-  rating?:number;
+  steam_id?:string;
 
-  rank?:number;
+  social?:{
+    twitch?:string;
+    youtube?:string;
+  };
+
+}
+
+
+export interface PlayerRating {
+
+  leaderboard:string;
+
+  rating:number;
+
+  rank:number;
+
+  rank_level:string;
+
+  games:number;
+
+  wins:number;
+
+  losses:number;
+
+}
+
+
+export interface PlayerProfile
+extends AOE4Player {
+
+  ratings?:PlayerRating[];
 
 }
