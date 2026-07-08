991dc26331d5da19fe2270eb62cf4b509bbd99d3
billevans <bill.sunshine@gmail.com>
2026-07-08 22:04:59 +1000

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,24 +1,24 @@
-export const APP_CONFIG = {
-  name: "ELO Trail",
-  description:
-    "Track Age of Empires IV player ELO history over time"
-};
-
 export const AOE4WORLD = {
+
   API_URL:
     process.env.AOE4WORLD_API_URL ??
     "https://aoe4world.com/api/v0",
 
-  DEFAULT_GAME_MODE: "rm_solo",
+  DEFAULT_LEADERBOARD:
+    "rm_solo",
 
-  SUPPORTED_MODES: [
+  SUPPORTED_LEADERBOARDS:[
+
     {
-      id: "rm_solo",
-      name: "Ranked 1v1"
+      id:"rm_solo",
+      name:"Ranked Solo"
     },
+
     {
-      id: "rm_team",
-      name: "Ranked Team"
+      id:"rm_team",
+      name:"Ranked Team"
     }
+
   ]
+
 };
