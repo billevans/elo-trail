1dc0c853989795ef4c16fc22cf2bb0cfe56d3b91
billevans <bill.sunshine@gmail.com>
2026-07-08 21:24:54 +1000

    Create footer.tsx

---
+++ components/layout/footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer(){
+
+  return (
+
+    <footer
+      className="
+        border-t
+        border-white/10
+        py-6
+        text-center
+        text-sm
+        text-slate-400
+      "
+    >
+
+      ELO Trail · Age of Empires IV Analytics
+
+    </footer>
+
+  );
+
+}
