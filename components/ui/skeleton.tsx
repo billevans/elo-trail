475ac61acad714575fa6a0f985906ae1c3a25773
billevans <bill.sunshine@gmail.com>
2026-07-08 21:40:04 +1000

    Create skeleton.tsx

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,39 @@
+import {
+  cn
+} from "@/lib/utils";
+
+
+interface SkeletonProps {
+
+  className?:string;
+
+}
+
+
+export function Skeleton({
+
+  className
+
+}:SkeletonProps){
+
+  return (
+
+    <div
+
+      className={cn(
+
+        `
+        animate-pulse
+        rounded-lg
+        bg-white/10
+        `,
+
+        className
+
+      )}
+
+    />
+
+  );
+
+}
